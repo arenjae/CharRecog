cb16943d8a777255e8306dae3cd970c10593d415
earthshine0 <earthshinelynx@gmail.com>
2016-08-20 11:40:42 -0700

    writing neural network in javascript

--- js/neural.js
+++ js/neural.js
@@ -2,29 +2,34 @@
  * Created by rae on 8/19/16.
  */
 
-var nj = require('numjs');
+var synaptic = require('synaptic');
 
 class Network{
 
     constructor(biases,weights){
+        this.num_layers = len(sizes);
         this.biases = biases;
         this.weights = weights;
+        this.sizes = sizes;
+
+        this.network = new synaptic.Architect.Perceptron(40, 25, 3);
     }
 
     feedforward(a){
-
+        for (var b = 0, w=0; b<length(self.biases); b++, w++){
+            a = sigmoid(math.dot(self.weights[w],a)+self.biases[b]);
+        }
+        return a;
     }
 
 
 }
 
 
-
-
-function sigmoid() {
-    return 1.0/(1.0+np.exp(-z));
+function sigmoid(z) {
+    return 1.0/(1.0+exp(-z));
 }
 
 function sigmoid_prime(z){
-    return this.sigmoid(z)*(1-this.sigmoid(z));
+    return sigmoid(z)*(1-sigmoid(z));
 }
