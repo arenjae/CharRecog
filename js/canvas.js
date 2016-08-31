9db11b922e8684b23359f6a731a086fc3acd90c4
earthshine0 <earthshinelynx@gmail.com>
2016-08-30 22:10:05 -0700

    writing neural network in javascript

--- js/canvas.js
+++ js/canvas.js
@@ -2,14 +2,19 @@
  * Created by rae on 8/19/16.
  */
 
+var $canvas;
+var ctx;
 var paint;
 var clickX = [];
 var clickY = [];
 var clickDrag = [];
-var context = document.getElementById('drawpad').getContext("2d");
-document.getElementById('drawpad').style.cursor = "crosshair";
 
+
+
+document.getElementById('drawpad').style.cursor = "crosshair";
 $canvas = $('#drawpad');
+ctx = document.getElementById('drawpad').getContext('2d');
+
 
 $canvas.mousedown(function(e){
     paint = true;
@@ -27,6 +32,7 @@ $canvas.mousemove(function(e){
 
 $canvas.mouseup(function(){
     paint = false;
+    clickDrag = [];
 });
 
 $canvas.mouseleave(function(){
@@ -36,93 +42,35 @@ $canvas.mouseleave(function(){
 
 function addClick(x, y, dragging)
 {
-    clickX.push(x);
-    clickY.push(y);
+    clickX[0]=clickX[1];
+    clickY[0]=clickY[1];
+    clickX[1]=x;
+    clickY[1]=y;
     clickDrag.push(dragging);
 }
 
 function clearCanvas(){
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clears the canvas
+    document.getElementById('output').innerHTML='';
 }
 
 function redraw(){
 
-    context.strokeStyle = '#000000';
-    context.lineJoin = "round";
-    context.lineCap = "round";
-    context.lineWidth = 5;
-
-    for(var i=0; i < clickX.length; i++) {
-        context.beginPath();
-        if(clickDrag[i] && i){
-            context.moveTo(clickX[i-1], clickY[i-1]);
-        }else{
-            context.moveTo(clickX[i]-1, clickY[i]);
-        }
-        context.lineTo(clickX[i], clickY[i]);
-        context.closePath();
-        context.stroke();
-    }
+    ctx.beginPath();
+    ctx.strokeStyle = '#000000';
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.lineWidth = 5;
+    ctx.moveTo(clickX[0], clickY[0]);
+    ctx.lineTo(clickX[1], clickY[1]);
+    ctx.stroke();
+    ctx.closePath();
 }
 
 function evaluateCanvas(){
-    // Save canvas as a bitmap
-    // resize to 28 x 28 , convert to numpy array.
-    // get biases and weights from text files
-    // pass to network
-
-    // $.get('values/weights.txt',function(data){
-    //     weights = data.split('],');
-    // });
-
-
-
-    // var biases = [[[ 0.75367042],
-    //     [-1.98232903],
-    //     [-6.02438455],
-    //     [-1.11645684],
-    //     [-0.51486567],
-    //     [-1.13614348],
-    //     [ 0.87759401],
-    //     [-2.29658651],
-    //     [-1.93875778],
-    //     [ 2.64916244],
-    //     [-0.01959318],
-    //     [ 3.75780427],
-    //     [-3.82270653],
-    //     [-0.99194332],
-    //     [-4.3617881 ],
-    //     [ 1.71847388],
-    //     [-1.88656953],
-    //     [ 0.35700252],
-    //     [-5.6053405 ],
-    //     [ 0.56107512],
-    //     [ 1.22460975],
-    //     [-1.76579728],
-    //     [ 0.40221091],
-    //     [ 1.17055423],
-    //     [ 2.9019542 ],
-    //     [-1.16828007],
-    //     [ 2.80983109],
-    //     [-2.71561958],
-    //     [-5.99988858],
-    //     [-2.02860761]],
     //
-    //     [[-1.17558266],
-    //         [-3.78425333],
-    //         [-4.94751098],
-    //         [-7.84785918],
-    //         [-7.72568523],
-    //         [-1.88004918],
-    //         [-0.3465647 ],
-    //         [-6.25915482],
-    //         [-7.23179592],
-    //         [-1.38264134]]];
-
-    // var weights = $.get('values/weights.txt');
-
-    var dataURL = document.getElementById('drawpad').toDataURL("image/jpeg",1.0);
-    var net = new Network();
-    $('#output').html(net.evaluate(dataURL));
+    // var dataURL = document.getElementById('drawpad').toDataURL("image/jpeg",1.0);
+    // var net = new Network();
+    // $('#output').html(net.evaluate(dataURL));
 
 }
